08eab017f2167e7c3048762c27add189e971dcd9
Shehbaz Khan <baz6066@gmail.com>
2019-01-12 18:49:55 -0600

    Added NewWin.h - removes unused windows defines and adds the target Windows version

---
+++ Text-editor/NewWin.h
@@ -0,0 +1,49 @@
+/*
+	The contents of the file are originally taken from https://github.com/planetchili/hw3d/tree/master/hw3d
+*/
+#pragma once
+
+// target Windows 7 or later
+#define _WIN32_WINNT 0x0601
+#include <sdkddkver.h>
+
+// Removes bunch of unused windows defines that may cause windows app to not function as desired
+
+#define WIN32_LEAN_AND_MEAN
+#define NOGDICAPMASKS
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOKERNEL
+#define NONLS
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOMINMAX
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#define NORPC
+#define NOPROXYSTUB
+#define NOIMAGE
+#define NOTAPE
+
+#define STRICT
+
+#include <Windows.h>

1ffa32f7ac2d974e10e56dd8113e7ed451460e2b
Shehbaz Khan <baz6066@gmail.com>
2019-01-17 16:28:11 -0600

    Moved some of the '#define' to the top in resource.h so they do not get overwritten. Removed unwanted debug statement.

--- Text-editor/DialogBoxWindow.cpp
+++ Text-editor/DialogBoxWindow.cpp
@@ -20,9 +20,6 @@ BOOL CALLBACK DialogBoxWindow::AboutDlgProc(HWND hWnd, UINT uMsg, WPARAM wParam,
 		int screenWidth = GetSystemMetrics(SM_CXSCREEN);
 		int screenHeight = GetSystemMetrics(SM_CYSCREEN);
 
-		std::string output = "LEFT: " + std::to_string(wRect.left) + ", TOP: " + std::to_string(wRect.top) +
-			", RIGHT: " + std::to_string(wRect.right) + ", BOTTOM: " + std::to_string(wRect.bottom);
-
 		SetWindowText(hWnd, dialogBoxtitle);
 		SetWindowPos(hWnd, NULL, (screenWidth - wRect.right) / 2, (screenHeight - wRect.bottom) / 2, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 
--- Text-editor/resource.h
+++ Text-editor/resource.h
@@ -2,25 +2,19 @@
 // Microsoft Visual C++ generated include file.
 // Used by Text-editor.rc
 //
+
+#define DEBUG                           1
+#define TOP_MARGIN                      6
+#define LEFT_MARGIN                     7
+#define TAB_LIMIT                       8
+#define MAX_LIMIT						0x7FFFFFFE //2147483646 character limit
+
 #define IDD_DIALOG1                     101
 #define IDC_MAIN_EDIT                   102
 #define ID_FILE_OPEN                    103
 #define ID_FILE_SAVEAS                  104
 #define ID_FILE_EXIT                    105
 #define ID_HELP_ABOUT                   106
-#define IDC_COMBO1                      1001
-
-#define TOP_MARGIN                      6
-#define LEFT_MARGIN                     7
-#define TAB_LIMIT                       8
-#define MAX_LIMIT						0x7FFFFFFE //2147483646 character limit
-#define DEBUG							1
-
-#if DEBUG
-#include <iostream>
-#include <string>
-#define LOG(x) OutputDebugString(x)
-#endif
 
 // Next default values for new objects
 // 
@@ -28,7 +22,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
